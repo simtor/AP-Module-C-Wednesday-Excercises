363733d70d41af74e1fc3e1efd1d6115a73ea849
Simone Ram <31472658+simtor@users.noreply.github.com>
2021-01-14 21:41:30 +0000

    Update Exercise2.cpp

--- Exercise2.cpp
+++ Exercise2.cpp
@@ -1,1 +1,39 @@
-//File Writer
+//Exercise 2: File Writer
+
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+string input;
+string decison;
+
+
+int main () {
+  cout << "Hello, Welcome to File Writer\n";
+
+  cout<<"Enter name: ";
+  // cin>>input;
+  getline(cin, input);
+
+  ofstream myfile;
+  myfile.open ("list.csv");
+  cout<<"Updating file... \n";
+  myfile << input <<"\n";
+  myfile.close();
+
+  cout<< "Add another (y/n)?";
+  // cin>>decison;
+  getline(cin, decison);
+  if(decison=="Y"||decison=="y"){
+    myfile.close();
+    main();
+  }else if(decison=="N"||decison=="n"){
+    cout<<"Goodbye";
+  }else{ 
+
+  }
+
+  
+  return 0;
+}
