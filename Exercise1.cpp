ef6b89179cf076e36751734b599924a2eb97ddc1
Simone Ram <31472658+simtor@users.noreply.github.com>
2021-01-14 21:40:38 +0000

    Update Exercise1.cpp

--- Exercise1.cpp
+++ Exercise1.cpp
@@ -1,1 +1,26 @@
-//File Exists
+//Exercise 1: File Exists
+//importing
+#include <iostream>
+using namespace std;
+#include <sys/stat.h>
+#include <string>
+
+string input;
+struct stat file;
+
+int main (){
+  cout << "Hello, Welcome to File Exists\n";
+  cout<< "Filename to check: ";
+  cin>>input;
+
+  if(stat(input.c_str(), &file) != 0) {
+     cout << input << " does not exist \n";
+     main();
+  }else{
+  cout << input << file.st_size << " bytes,last modifield: " << ctime(&file.st_mtime);
+  }
+  return 0;
+
+}
+
+
