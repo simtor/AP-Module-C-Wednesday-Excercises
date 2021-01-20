31d72d8a8c02de68b1a1382dfd71a017e399aa38
Simone Ram <31472658+simtor@users.noreply.github.com>
2021-01-20 13:54:41 +0000

    Added comments to the code

--- exercise1/Exercise 1.cpp
+++ exercise1/Exercise 1.cpp
@@ -5,18 +5,21 @@ using namespace std;
 #include <sys/stat.h>
 #include <string>
 
-string input;
+string input;//declared variable to used for input 
 struct stat file;
 
 int main (){
   cout << "Hello, Welcome to File Exists\n";
   cout<< "Filename to check: ";
-  cin>>input;
+  cin>>input;//Getting input from the user 
 
+//if statement used to check if the file exist 
   if(stat(input.c_str(), &file) != 0) {
      cout << input << " does not exist \n";
+     //message if the file doesn't noe exist
      main();
   }else{
+    //if the file exist th eprogrma would output the size 
   cout << input << file.st_size << " bytes,last modifield: " << ctime(&file.st_mtime);
   }
   return 0;
